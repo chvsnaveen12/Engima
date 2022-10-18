5c315a8607cb33e4cde8d58cd9f0013c9d2016e1
chvsnaveen12 <90030628+chvsnaveen12@users.noreply.github.com>
2022-10-18 18:24:02 +0530

    Add files via upload

---
+++ ENGIMA/ENGIMA.ino
@@ -0,0 +1,549 @@
+
+int x;
+int y0=0;
+int y=0;
+int a=0;
+int b=0;
+int c=0;
+
+
+int light_a = 52;
+int light_b = 53;
+int light_c = 2;
+int light_d = 3;
+int light_e = 4;
+int light_f = 5;
+int light_g = 6;
+int light_h = 7;
+int light_i = 8;
+int light_j = 9;
+int light_k = 10;
+int light_l = 11;
+int light_m = 12;
+int light_n = 13;
+int light_o = 14;
+int light_p = 15;
+int light_q = 16;
+int light_r = 17;
+int light_s = 18;
+int light_t = 19;
+int light_u = 20;
+int light_v = 21;
+int light_w = 22;
+int light_x = 23;
+int light_y = 24;
+int light_z = 25;
+
+
+
+int button_a = 26;
+int button_b = 27;
+int button_c = 28;
+int button_d = 29;
+int button_e = 30;
+int button_f = 31;
+int button_g = 32;
+int button_h = 33;
+int button_i = 34;
+int button_j = 35;
+int button_k = 36;
+int button_l = 37;
+int button_m = 38;
+int button_n = 39;
+int button_o = 40;
+int button_p = 41;
+int button_q = 42;
+int button_r = 43;
+int button_s = 44;
+int button_t = 45;
+int button_u = 46;
+int button_v = 47;
+int button_w = 48;
+int button_x = 49;
+int button_y = 50;
+int button_z = 51;
+
+void setup() {
+    
+
+  pinMode(light_a, OUTPUT);
+  pinMode(light_b, OUTPUT);
+  pinMode(light_c, OUTPUT);
+  pinMode(light_d, OUTPUT);
+  pinMode(light_e, OUTPUT);
+  pinMode(light_f, OUTPUT);
+  pinMode(light_g, OUTPUT);
+  pinMode(light_h, OUTPUT);
+  pinMode(light_i, OUTPUT);
+  pinMode(light_j, OUTPUT);
+  pinMode(light_k, OUTPUT);
+  pinMode(light_l, OUTPUT);
+  pinMode(light_m, OUTPUT);
+  pinMode(light_n, OUTPUT);
+  pinMode(light_o, OUTPUT);
+  pinMode(light_p, OUTPUT);
+  pinMode(light_q, OUTPUT);
+  pinMode(light_r, OUTPUT);
+  pinMode(light_s, OUTPUT);
+  pinMode(light_t, OUTPUT);
+  pinMode(light_u, OUTPUT);
+  pinMode(light_v, OUTPUT);
+  pinMode(light_w, OUTPUT);
+  pinMode(light_x, OUTPUT);
+  pinMode(light_y, OUTPUT);
+  pinMode(light_z, OUTPUT);
+
+  pinMode(button_a, INPUT);
+  pinMode(button_b, INPUT);
+  pinMode(button_c, INPUT);
+  pinMode(button_d, INPUT);
+  pinMode(button_e, INPUT);
+  pinMode(button_f, INPUT);
+  pinMode(button_g, INPUT);
+  pinMode(button_h, INPUT);
+  pinMode(button_i, INPUT);
+  pinMode(button_j, INPUT);
+  pinMode(button_k, INPUT);
+  pinMode(button_l, INPUT);
+  pinMode(button_m, INPUT);
+  pinMode(button_n, INPUT);
+  pinMode(button_o, INPUT);
+  pinMode(button_p, INPUT);
+  pinMode(button_q, INPUT);
+  pinMode(button_r, INPUT);
+  pinMode(button_s, INPUT);
+  pinMode(button_t, INPUT);
+  pinMode(button_u, INPUT);
+  pinMode(button_v, INPUT);
+  pinMode(button_w, INPUT);
+  pinMode(button_x, INPUT);
+  pinMode(button_y, INPUT);
+  pinMode(button_z, INPUT);
+Serial.begin(9600);}
+void loop() {
+  
+  int x0=0;
+   int x1=0;
+  int x2=0;
+  int x3=0;
+  int x4=0;
+  int x5=0;
+  int x6=0;
+  int x7=0;
+  int x8=0;
+  int x9=0;
+  int x10=0;
+  int x11=0;
+  int x12=0;
+  int x13=0;
+  int x14=0;
+  int x15=0;
+  int x16=0;
+  int x17=0;
+  int x18=0;
+  int x19=0;
+  int x20=0;
+  int x21=0;
+  int x22=0;
+  int x23=0;
+  int x24=0;
+  int x25=0;
+  
+  
+       if (digitalRead(button_a) ==1)
+       x0 = 0;
+       else
+       x0 = 100;
+    
+     if (digitalRead(button_b) == 1)
+      x1 = 1;
+      else
+    x1 = 100;
+if(digitalRead(button_c)==HIGH)
+      x2 = 2;
+      else
+      x2 = 100;
+      
+       if(digitalRead(button_d)==HIGH)
+      x3 = 3;
+      else
+      x3 = 100;
+      
+       if(digitalRead(button_e)==HIGH)
+      x4 = 4;
+      else
+      x4 = 100;
+      
+       if(digitalRead(button_f)==HIGH)
+      x5 = 5;
+      else
+      x5 = 100;
+      
+        
+     if(digitalRead(button_g)==HIGH)
+      x6 = 6;
+      else
+      x6 = 100;
+      
+       if(digitalRead(button_h)==HIGH)
+      x7 = 7;
+      else
+      x7 = 100;
+      
+       if(digitalRead(button_i)==HIGH)
+      x8 = 8;
+      else
+      x8 = 100;
+      
+       if(digitalRead(button_j)==HIGH)
+      x9 = 9;
+      else
+      x9 = 100;
+      
+        
+     if(digitalRead(button_k)==HIGH)
+      x10 = 10;
+      else
+      x10 = 100;
+      
+       if(digitalRead(button_l)==HIGH)
+      x11 = 11;
+      else
+      x11 = 100;
+      
+       if(digitalRead(button_m)==HIGH)
+      x12 = 12;
+      else
+     x12 = 100;
+      
+       if(digitalRead(button_n)==HIGH)
+      x13 = 13;
+      else
+      x13 = 100;
+      
+        
+     if(digitalRead(button_o)==HIGH)
+      x14 = 14;
+      else
+      x14 = 100;
+      
+       if(digitalRead(button_p)==HIGH)
+      x15 = 15;
+      else
+      x15 = 100;
+      
+      
+      if(digitalRead(button_q)==HIGH)
+      x16 = 16;
+      else
+      x16 = 100;
+      
+       if(digitalRead(button_r)==HIGH)
+      x17 = 17;
+      else
+      x17 = 100;
+      
+        
+     if(digitalRead(button_s)==HIGH)
+      x18 = 18;
+      else
+      x18 = 100;
+      
+       if(digitalRead(button_t)==HIGH)
+      x19 = 19;
+      else
+      x19 = 100;
+      
+       if(digitalRead(button_u)==HIGH)
+      x20 = 20;
+      else
+      x20 = 100;
+      
+       if(digitalRead(button_v)==HIGH)
+      x21 = 21;
+      else
+      x21 = 100;
+      
+        
+     if(digitalRead(button_w)==HIGH)
+      x22 = 22;
+      else
+      x22 = 100;
+      
+       if(digitalRead(button_x)==HIGH)
+      x23 = 23;
+      else
+      x23 = 100;
+      
+       if(digitalRead(button_y)==HIGH)
+      x24 = 24;
+      else
+      x24 = 100;
+      
+       if(digitalRead(button_z)==HIGH)
+      x25 = 25;
+      else
+      x25 = 100;
+      
+
+if (x0<100||x1<100||x2<100||x3<100||x4<100||x5<100||x6<100||x7<100||x8<100||x9<100||x10<100||x11<100||x12<100||x13<100||x14<100||x15<100||x16<100||x17<100||x18<100||x19<100||x20<100||x21<100||x22<100||x23<100||x24<100||x25<100){
+  Serial.println("hello");
+  if(x0==1)
+ y0=findvalue(0,a,b,c);
+if(y0==0) digitalWrite(light_a,HIGH);  else digitalWrite(light_a,LOW);  if(y0==1) digitalWrite(light_b,HIGH); else digitalWrite(light_b,LOW);  if(y0==2) digitalWrite(light_c,HIGH); else digitalWrite(light_c,LOW);  if(y0==3) digitalWrite(light_d,HIGH); else digitalWrite(light_d,LOW);  if(y0==4) digitalWrite(light_e,HIGH); else digitalWrite(light_e,LOW);  if(y0==5) digitalWrite(light_f,HIGH); else digitalWrite(light_f,LOW);  if(y0==6) digitalWrite(light_g,HIGH); else digitalWrite(light_g,LOW);  if(y0==7) digitalWrite(light_h,HIGH); else digitalWrite(light_h,LOW);  if(y0==8) digitalWrite(light_i,HIGH); else digitalWrite(light_i,LOW);  if(y0==9) digitalWrite(light_j,HIGH); else digitalWrite(light_j,LOW);  if(y0==10) digitalWrite(light_k,HIGH); else digitalWrite(light_k,LOW);  if(y0==11) digitalWrite(light_l,HIGH); else digitalWrite(light_l,LOW);  if(y0==12) digitalWrite(light_m,HIGH); else digitalWrite(light_m,LOW);  if(y0==13) digitalWrite(light_n,HIGH); else digitalWrite(light_n,LOW);  if(y0==14) digitalWrite(light_o,HIGH); else digitalWrite(light_o,LOW);  if(y0==15) digitalWrite(light_p,HIGH); else digitalWrite(light_p,LOW);  if(y0==16) digitalWrite(light_q,HIGH); else digitalWrite(light_q,LOW);  if(y0==17) digitalWrite(light_r,HIGH); else digitalWrite(light_r,LOW);  if(y0==18) digitalWrite(light_s,HIGH); else digitalWrite(light_s,LOW);  if(y0==19) digitalWrite(light_t,HIGH); else digitalWrite(light_t,LOW);  if(y0==20) digitalWrite(light_u,HIGH); else digitalWrite(light_u,LOW);  if(y0==21) digitalWrite(light_v,HIGH); else digitalWrite(light_v,LOW);  if(y0==22) digitalWrite(light_w,HIGH); else digitalWrite(light_w,LOW);  if(y0==23) digitalWrite(light_x,HIGH); else digitalWrite(light_x,LOW);  if(y0==24) digitalWrite(light_y,HIGH); else digitalWrite(light_y,LOW);  if(y0==25) digitalWrite(light_z,HIGH); else digitalWrite(light_z,LOW);
+
+if(x1==1)
+y=findvalue(1,a,b,c);
+if(x2==1)
+y=findvalue(2,a,b,c);
+if(x3==1)
+y=findvalue(3,a,b,c);
+if(x4==1)
+y=findvalue(4,a,b,c);
+if(x5==1)
+y=findvalue(5,a,b,c);
+if(x6==1)
+y=findvalue(6,a,b,c);
+if(x7==1)
+y=findvalue(7,a,b,c);
+if(x8==1)
+y=findvalue(8,a,b,c);
+if(x9==1)
+y=findvalue(9,a,b,c);
+if(x10==1)
+y=findvalue(10,a,b,c);
+if(x11==1)
+y=findvalue(11,a,b,c);
+if(x12==1)
+y=findvalue(12,a,b,c);
+if(x13==1)
+y=findvalue(13,a,b,c);
+if(x14==1)
+y=findvalue(14,a,b,c);
+if(x15==1)
+y=findvalue(15,a,b,c);
+if(x16==1)
+y=findvalue(16,a,b,c);
+if(x17==1)
+y=findvalue(17,a,b,c);
+if(x18==1)
+y=findvalue(18,a,b,c);
+if(x19==1)
+y=findvalue(19,a,b,c);
+if(x20==1)
+y=findvalue(20,a,b,c);
+if(x21==1)
+y=findvalue(21,a,b,c);
+if(x22==1)
+y=findvalue(22,a,b,c);
+if(x23==1)
+y=findvalue(23,a,b,c);
+if(x24==1)
+y=findvalue(24,a,b,c);
+if(x25==1)
+y=findvalue(25,a,b,c);
+
+a++;
+if(a>=26){
+a%26;
+b++;}
+
+
+
+if(y==0)
+digitalWrite(light_a,HIGH);
+
+else
+digitalWrite(light_a,LOW);
+
+if(y==1)
+digitalWrite(light_b,HIGH);
+else
+digitalWrite(light_b,LOW);
+
+if(y==2)
+digitalWrite(light_c,HIGH);
+else
+digitalWrite(light_c,LOW);
+
+if(y==3)
+digitalWrite(light_d,HIGH);
+else
+digitalWrite(light_d,LOW);
+
+if(y==4)
+digitalWrite(light_e,HIGH);
+else
+digitalWrite(light_e,LOW);
+
+if(y==5)
+digitalWrite(light_f,HIGH);
+else
+digitalWrite(light_f,LOW);
+
+if(y==6)
+digitalWrite(light_g,HIGH);
+else
+digitalWrite(light_g,LOW);
+
+if(y==7)
+digitalWrite(light_h,HIGH);
+else
+digitalWrite(light_h,LOW);
+
+if(y==8)
+digitalWrite(light_i,HIGH);
+else
+digitalWrite(light_i,LOW);
+
+if(y==9)
+digitalWrite(light_j,HIGH);
+else
+digitalWrite(light_j,LOW);
+
+if(y==10)
+digitalWrite(light_k,HIGH);
+else
+digitalWrite(light_k,LOW);
+
+if(y==11)
+digitalWrite(light_l,HIGH);
+else
+digitalWrite(light_l,LOW);
+
+if(y==12)
+digitalWrite(light_m,HIGH);
+else
+digitalWrite(light_m,LOW);
+
+if(y==13)
+digitalWrite(light_n,HIGH);
+else
+digitalWrite(light_n,LOW);
+
+if(y==14)
+digitalWrite(light_o,HIGH);
+else
+digitalWrite(light_o,LOW);
+
+if(y==15)
+digitalWrite(light_p,HIGH);
+else
+digitalWrite(light_p,LOW);
+
+if(y==16)
+digitalWrite(light_q,HIGH);
+else
+digitalWrite(light_q,LOW);
+
+if(y==17)
+digitalWrite(light_r,HIGH);
+else
+digitalWrite(light_r,LOW);
+
+if(y==18)
+digitalWrite(light_s,HIGH);
+else
+digitalWrite(light_s,LOW);
+
+if(y==19)
+digitalWrite(light_t,HIGH);
+else
+digitalWrite(light_t,LOW);
+
+if(y==20)
+digitalWrite(light_u,HIGH);
+else
+digitalWrite(light_u,LOW);
+
+if(y==21)
+digitalWrite(light_v,HIGH);
+else
+digitalWrite(light_v,LOW);
+
+if(y==22)
+digitalWrite(light_w,HIGH);
+else
+digitalWrite(light_w,LOW);
+
+if(y==23)
+digitalWrite(light_x,HIGH);
+else
+digitalWrite(light_x,LOW);
+
+if(y==24)
+digitalWrite(light_y,HIGH);
+else
+digitalWrite(light_y,LOW);
+
+if(y==25)
+digitalWrite(light_z,HIGH);
+else
+digitalWrite(light_z,LOW);
+delay(1000);
+}
+}
+
+ int findvalue(int value,int x , int y, int z){
+ 
+  int rotor1[26] = {10,7,4,17,15,24,21,19,3,1,13,9,6,18,22,20,16,14,5,23,11,2,12,26,25,8};
+  int   inverserotor1[26] = {10,22,9,3,19,13,2,26,12,1,21,23,11,18,5,17,4,14,8,16,7,15,20,6,25,24};
+   int rotor2[26] = {14,20,26,16,19,6,2,15,11,13,23,18,3,10,4,9,22,12,1,5,25,21,24,8,7,17};
+   int inverserotor2[26] = {19,7,13,15,20,6,25,24,16,14,9,18,10,1,8,4,26,12,5,2,22,17,11,23,21,3};
+    int rotor3[26] = {10,22,9,21,2,8,20,3,4,25,1,11,5,17,26,16,15,19,7,24,14,18,13,23,6,12};
+   int inverserotor3[26] = {11,5,8,9,13,25,19,6,3,1,12,26,23,21,17,16,14,22,18,7,4,2,24,20,10,15};
+   int reflector[26] = {5,10,13,26,1,12,25,24,22,2,23,6,3,18,17,21,15,14,20,19,16,9,11,8,7,4};
+  
+   int temp1 = (value+x)%26;
+   int outputrotor1 = rotor1[temp1]-1; 
+ 
+   
+   int temp2 = (outputrotor1+(y-x))%26;
+   if(temp2<0)
+   temp2=temp2+26;
+   int outputrotor2 = rotor2[temp2]-1;
+   
+   
+   int temp3 = (outputrotor2+(z-y))%26;
+   if(temp3<0)
+   temp3=temp3+26;
+   int outputrotor3 = rotor3[temp3]-1;
+ 
+   
+  int reflectortemp =(outputrotor3-z)%26;
+  if(reflectortemp<0)
+   reflectortemp=reflectortemp+26;
+   int outputreflector = reflector[reflectortemp]-1; 
+  
+   int inversetemp1 = (outputreflector+z)%26;
+   int outputinverserotor3 = inverserotor3[inversetemp1]-1;
+   
+   
+   int inversetemp2 = (outputinverserotor3+(y-z))%26;
+   if(inversetemp2<0)
+   inversetemp2=inversetemp2+26;
+   int outputinverserotor2 = inverserotor2[inversetemp2]-1;
+ 
+   
+   
+  int inversetemp3 = (outputinverserotor2+(x-y)) % 26;
+   if(inversetemp3<0)
+   inversetemp3=inversetemp3+26;
+   int outputinverserotor1 = inverserotor1[inversetemp3]-1;
+
+
+
+  
+   
+   int finaltemp = (outputinverserotor1-x);
+   if(finaltemp<0)
+   finaltemp=finaltemp+26;
+ 
+
+
+   int output = finaltemp;
+
+   return output;
+
+    
+}
